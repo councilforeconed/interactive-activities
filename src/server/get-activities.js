5fedddf7090496f60ecabb138bc26143c7c4ed2b
Mike Pennisi <mike@mikepennisi.com>
2014-02-26 13:08:51 -0500

    Validate Activity data and structure
    Promote early discovery of errors in Activity defintions.

--- src/server/get-activities.js
+++ src/server/get-activities.js
@@ -1,17 +1,45 @@
 var fs = require('fs');
+var path = require('path');
 
+var activitiesDir = path.join(__dirname, '../activities');
+
+/**
+ * Return an array with metadata for each activity as defined by the files
+ * present in the project's `src/activities/` directory. Additionally, perform
+ * some basic validation to ensure that each activity conforms to the
+ * expectations of the build process.
+ */
 module.exports = function() {
-  return fs.readdirSync('src/activities').map(function(dirName) {
+  return fs.readdirSync(activitiesDir).map(function(name) {
+    var directory = path.join(activitiesDir, name);
     return {
-      slug: dirName,
-      fileName: './src/activities/' + dirName + '/config.json'
+      slug: name,
+      directory: directory,
+      configFile: directory + '/config.json'
     };
   }).filter(function(activity) {
-    return fs.statSync(activity.fileName).isFile();
+    return fs.statSync(activity.configFile).isFile();
   }).map(function(activity) {
-    var metaDataJSON = String(fs.readFileSync(activity.fileName));
-    var metaData = JSON.parse(metaDataJSON);
-    metaData.slug = activity.slug;
-    return metaData;
+    var configJSON = String(fs.readFileSync(activity.configFile));
+    var config = JSON.parse(configJSON);
+    try {
+      validate(activity.directory, config);
+    } catch(err) {
+      err.message = 'Activity error: (' + activity.slug + ') '+ err.message;
+      throw err;
+    }
+    config.slug = activity.slug;
+    return config;
   });
 };
+
+function validate(directory, metaData) {
+  if ('slug' in metaData) {
+    throw new Error('The "slug" configuration parameter is reserved');
+  }
+  if (!fs.statSync(directory + '/client/scripts/main.js').isFile()) {
+    throw new Error(
+      'Missing "main.js" entry point in "client/scripts" directory'
+    );
+  }
+}
