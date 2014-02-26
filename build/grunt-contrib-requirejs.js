f74e2e8f08790236735ad1feb9a36e38fa818923
Mike Pennisi <mike@mikepennisi.com>
2014-02-26 12:36:31 -0500

    Programatically generate `modules` configuration

--- build/grunt-contrib-requirejs.js
+++ build/grunt-contrib-requirejs.js
@@ -19,30 +19,51 @@ module.exports = function(grunt) {
         pragmasOnSave: {
           excludeJade: true
         },
-        modules: [
-          {
-            name: 'scripts/main',
-            include: [
-              // Include RequireJS to support lazy loading of activities
-              '../../bower_components/requirejs/require',
-              // Include the configuration to ensure that:
-              // 1. the application initializes immediately (via the `deps`
-              //    option)
-              // 2. pathing information is consitent with development mode for
-              //    runtime dependency resolution (specifically in service of
-              //    lazily-loaded activities)
-              'scripts/amd-config'
-            ]
-          },
-          {
-            name: '../activities/money-creation/client/scripts/main',
-            exclude: ['scripts/main'],
-            insertRequire: ['../activities/money-creation/client/scripts/main']
-          }
-        ]
+        modules: generateRjsModules()
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-requirejs');
 };
+
+/**
+ * In order to seamlessly optimize any and all valid activities, the `modules`
+ * configuration for the r.js optimizer must be programatically generated.
+ *
+ * @returns {Array} Configuration data for each module that should be
+ *                  optimized. See the r.js documentation for details on the
+ *                  format of this data:
+ *                  http://requirejs.org/docs/optimization.html
+ */
+function generateRjsModules() {
+  var activities = require('../src/server/get-activities')();
+  var mainModule = {
+    name: 'scripts/main',
+    include: [
+      // Include RequireJS to support lazy loading of activities
+      '../../bower_components/requirejs/require',
+      // Include the configuration to ensure that:
+      // 1. the application initializes immediately (via the `deps`
+      //    option)
+      // 2. pathing information is consitent with development mode for
+      //    runtime dependency resolution (specifically in service of
+      //    lazily-loaded activities)
+      'scripts/amd-config'
+    ]
+  };
+  var activityModules = activities.map(function(activity) {
+    return {
+      name: '../activities/' + activity.slug + '/client/scripts/main',
+      // Activities will only be run in the context of the site application, so
+      // optimized builds should omit any modules required by the "main"
+      // module.
+      exclude: ['scripts/main'],
+      insertRequire: [
+        '../activities/' + activity.slug + '/client/scripts/main'
+      ]
+    };
+  });
+
+  return [mainModule].concat(activityModules);
+}
