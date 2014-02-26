541e93c3cb864b7860d44d1d9f0c8252e2a50c76
Michael "Z" Goddard <mzgoddard@gmail.com>
2014-02-26 17:57:04 -0500

    Simple documentation for testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
@@ -17,6 +19,13 @@ module.exports = function(grunt) {
         src: ['src/index.js', 'src/server/*.js']
       },
 
+      server_activities: {
+        options: {
+          node: true
+        },
+        src: ['src/activities/*/{index,server/**/*}.js']
+      },
+
       server_test: {
         options: {
           node: true
@@ -34,4 +43,5 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('default', ['jshint', 'testem']);
+  grunt.registerTask('test', ['jshint', 'testem']);
 };
--- readme.md
+++ readme.md
@@ -2,3 +2,11 @@
 
 Interactive educational activities by Bocoup and the Council for Economic
 Education.
+
+## Setup
+
+1. `npm install`
+
+## Testing
+
+1. `grunt test`
