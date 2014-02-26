e9f47bfa513a7001eb520db895ec5b7dc284f1c2
Michael "Z" Goddard <mzgoddard@gmail.com>
2014-02-26 14:51:37 -0500

    Add and organize dependencies. Setup baseline grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,36 @@
 module.exports = function(grunt) {
-  grunt.registerTask('default', []);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    jshint: {
+      server: {
+        options: {
+          node: true
+        },
+        src: ['src/index.js', 'src/server/*.js']
+      }
+    },
+
+    testem: {
+      server: {
+        options: {
+          launchers: {
+            Mocha: {
+              command: [
+                'node_modules/.bin/mocha',
+                '--ui tdd',
+                '--reporter tap',
+                'test/server/test_*.js'
+              ].join(' '),
+              protocol: 'tap'
+            }
+          },
+          launch_in_ci: ['Mocha']
+        },
+        src: 'test/server/*.html'
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['jshint', 'testem']);
 };
