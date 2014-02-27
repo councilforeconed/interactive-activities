79281197fe9ab59dfe9fe6e56d48ef6bf3ed9878
Mike Pennisi <mike@mikepennisi.com>
2014-02-27 17:06:19 -0500

    Remove `copy` task in favor of Require.js
    This approach allows for the optimmization of shared library code. It is
    also slightly less fragile because all JavaScript asset building is
    handled within a single task.

--- build/grunt-contrib-requirejs.js
+++ build/grunt-contrib-requirejs.js
@@ -1,6 +1,16 @@
 'use strict';
 
 module.exports = function(grunt) {
+  // Library code that is shared between some activities (but not the
+  // top-level application) is optimized into distinct modules. Although this
+  // forces some activities to require multiple JavaScript files in
+  // production, it also:
+  // 1. minimizes the size of each individual activity
+  // 2. avoids the need to re-define library code between activities
+  var sharedLibraries = {
+    'socket.io': 'socket.io-client/dist/socket.io'
+  };
+
   grunt.config.set('requirejs', {
     prod: {
       options: {
@@ -22,7 +32,15 @@ module.exports = function(grunt) {
         pragmasOnSave: {
           excludeJade: true
         },
-        modules: generateRjsModules()
+        modules: generateRjsModules(sharedLibraries)
+      }
+    },
+    bower_components: {
+      options: {
+        baseUrl: 'bower_components',
+        dir: 'out/bower_components',
+        skipDirOptimize: true,
+        modules: getModuleDescriptors(sharedLibraries)
       }
     }
   });
@@ -34,12 +52,17 @@ module.exports = function(grunt) {
  * In order to seamlessly optimize any and all valid activities, the `modules`
  * configuration for the r.js optimizer must be programatically generated.
  *
+ * @param {Object} sharedLibraries A collection of shared library paths
+ *                 (relative to the `bower_components` directory), keyed on the
+ *                 library's module ID. These will be excluded from all
+ *                 activity modules for efficiency (see above for more detail).
+ *
  * @returns {Array} Configuration data for each module that should be
  *                  optimized. See the r.js documentation for details on the
  *                  format of this data:
  *                  http://requirejs.org/docs/optimization.html
  */
-function generateRjsModules() {
+function generateRjsModules(sharedLibraries) {
   var activities = require('../src/server/get-activities')();
   var mainModule = {
     name: 'scripts/main',
@@ -61,9 +84,15 @@ function generateRjsModules() {
       // Activities will only be run in the context of the site application, so
       // optimized builds should omit any modules required by the "main"
       // module.
-      exclude: ['scripts/main', 'socket.io']
+      exclude: ['scripts/main'].concat(Object.keys(sharedLibraries))
     };
   });
 
   return [mainModule].concat(activityModules);
+}
+
+function getModuleDescriptors(modulePaths) {
+  return Object.keys(modulePaths).map(function(id) {
+    return { name: modulePaths[id] };
+  });
 }
