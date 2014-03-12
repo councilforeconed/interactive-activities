76ad3ac01a53dad119d87ba1664873510c5817f8
Mike Pennisi <mike@mikepennisi.com>
2014-03-12 19:27:50 -0400

    Implement Slider component
    This component serves as an adapter over the third-party library
    responsible for rendering a "slider" UI (minimizing coupling with
    third-party code).

---
+++ src/client/components/slider/slider.js
@@ -0,0 +1,72 @@
+define(function(require) {
+  'use strict';
+  var Layout = require('layoutmanager');
+  var _ = require('underscore');
+  require('rangeslider');
+
+  var requiredAttributes = [
+    'attr', 'label', 'max', 'min', 'model', 'step'
+  ];
+
+  var ControlView = Layout.extend({
+    className: 'slider',
+    template: require('jade!./slider'),
+    keep: true,
+
+    initialize: function(options) {
+      _.forEach(requiredAttributes, function(attr) {
+        if (!(attr in options)) {
+          throw new Error(
+              'Slider component must be initialized with a "' + attr +
+              '" option.'
+          );
+        }
+        this[attr] = options[attr];
+      },this);
+
+      this.format = options.format;
+
+      this.listenTo(this.model, 'change:' + this.attr, this.render);
+
+      this.handleSlide = _.bind(this.handleSlide, this);
+      this.handleSlideEnd = _.bind(this.handleSlideEnd, this);
+    },
+
+    afterRender: function() {
+      this.$('input[type="range"]').rangeslider({
+        polyfill: false,
+        onSlide: this.handleSlide,
+        onSlideEnd: this.handleSlideEnd
+      });
+    },
+
+    handleSlide: function(position, value) {
+      if (this.format) {
+        value = this.format(value);
+      }
+
+      this.$('.value').text(value);
+    },
+
+    handleSlideEnd: function(position, value) {
+      this.model.set(this.attr, value);
+    },
+
+    serialize: function() {
+      var templateData = _.pick(this, requiredAttributes);
+      var value = this.model.get(this.attr);
+
+      templateData.value = value;
+
+      if (this.format) {
+        templateData.formattedValue = this.format(value);
+      } else {
+        templateData.formattedValue = value;
+      }
+
+      return templateData;
+    }
+  });
+
+  return ControlView;
+});
