666edf1e479a5fd44e35ee3c2e9c7aa3ce45d767
Michael "Z" Goddard <mzgoddard@gmail.com>
2014-04-09 11:15:48 -0400

    Wrap formatters/usd in a suite to indicate that

--- test/unit/client/tests/scripts/formatters/usd.js
+++ test/unit/client/tests/scripts/formatters/usd.js
@@ -2,33 +2,35 @@ define(function(require) {
   'use strict';
   var formatUSD = require('scripts/formatters/usd');
 
-  test('zero', function() {
-    assert.equal(formatUSD(0), '$0.00');
-  });
-
-  test('fractional values', function() {
-    assert.equal(formatUSD(1.2), '$1.20');
-  });
-
-  test('rounding to nearest cent', function() {
-    assert.equal(formatUSD(0.005), '$0.01');
-  });
-
-  test('high precision', function() {
-    assert.equal(formatUSD(0.9999999999999), '$1.00');
-  });
-
-  test('negative values', function() {
-    assert.equal(formatUSD(-12.2), '-$12.20');
-  });
-
-  suite('comma insertion', function() {
-    test('thousands', function() {
-      assert.equal(formatUSD(1234), '$1,234.00');
+  suite('formatters/usd', function() {
+    test('zero', function() {
+      assert.equal(formatUSD(0), '$0.00');
     });
 
-    test('millions', function() {
-      assert.equal(formatUSD(12345678.9), '$12,345,678.90');
+    test('fractional values', function() {
+      assert.equal(formatUSD(1.2), '$1.20');
+    });
+
+    test('rounding to nearest cent', function() {
+      assert.equal(formatUSD(0.005), '$0.01');
+    });
+
+    test('high precision', function() {
+      assert.equal(formatUSD(0.9999999999999), '$1.00');
+    });
+
+    test('negative values', function() {
+      assert.equal(formatUSD(-12.2), '-$12.20');
+    });
+
+    suite('comma insertion', function() {
+      test('thousands', function() {
+        assert.equal(formatUSD(1234), '$1,234.00');
+      });
+
+      test('millions', function() {
+        assert.equal(formatUSD(12345678.9), '$12,345,678.90');
+      });
     });
   });
 });
