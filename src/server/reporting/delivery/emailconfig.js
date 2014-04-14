5d053476fabfcb73924a3235b29da075064d6569
Michael "Z" Goddard <mzgoddard@gmail.com>
2014-04-14 02:29:49 -0400

    Add report exports: download and email.

---
+++ src/server/reporting/delivery/emailconfig.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// TODO: This is an unfinished stub. For email to work this will need to be
+// improved on and likely given an added mechanic to allow an administrator
+// easier control in production.
+module.exports = {
+  from: 'me@to.you'
+};
