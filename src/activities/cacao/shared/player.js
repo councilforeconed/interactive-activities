20929a07a0037560937cd8d3f341a9e311f0ee51
Mike Pennisi <mike@mikepennisi.com>
2014-04-24 15:34:26 -0400

    Model the lesson's "target price" distribution

--- src/activities/cacao/shared/player.js
+++ src/activities/cacao/shared/player.js
@@ -2,9 +2,41 @@ define(function(require) {
   'use strict';
 
   var Backbone = require('backbone');
+  var _ = require('lodash');
+
   var config = require('json!./config.json');
-  var minPrice = config.targetPrice.min;
-  var priceRange = config.targetPrice.max - minPrice;
+  var targets = {};
+
+  function makeTargets(distribution) {
+    var targets = [];
+
+    _.forEach(distribution, function(count, priceStr) {
+      var price = parseInt(priceStr, 10);
+
+      // Target prices are explicitly required to be integers (as opposed to
+      // floating-point values) by the source material.
+      if (price !== +priceStr) {
+        throw new Error(
+          'CEE Cacao activity: Prices must be integer values. ' +
+          '(Could not cast "' + priceStr + '" to an integer.)'
+        );
+      } else if (price <= 0) {
+        throw new Error(
+          'CEE Cacao activity: Prices must be greater than 0. ' +
+          '(Encountered value "' + price + '".)'
+        );
+      }
+
+      while (count-- > 0) {
+        targets.push(price);
+      }
+    });
+
+    return targets;
+  }
+
+  targets.buyer = makeTargets(config.targetDistributions.buyer);
+  targets.seller = makeTargets(config.targetDistributions.seller);
 
   var Player = Backbone.Model.extend({
     defaults: {
@@ -13,18 +45,15 @@ define(function(require) {
     },
 
     /**
-     * Assign a random target price to the player.
+     * Assign a target price to the player.
      *
      * @returns {Player} the Player instance
      */
     assignTarget: function() {
-      this.set(
-        'targetPrice',
-        // Currently implemented as a uniform distribution across a
-        // configurable range, this heuristic will likely need revision to
-        // consistently model some market value.
-        minPrice + Math.round(Math.random() * priceRange)
-      );
+      var playerTargets = targets[this.get('role')];
+      var idx = Math.floor(Math.random() * playerTargets.length);
+
+      this.set('targetPrice', playerTargets[idx]);
 
       return this;
     }
