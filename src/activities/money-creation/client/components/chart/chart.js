80d26612210643f5a39c9fcc5b57ba17453685fd
Steve Kinney <hello@stevekinney.net>
2014-06-11 15:12:49 -0400

    Implement Money Creation Chart

---
+++ src/activities/money-creation/client/components/chart/chart.js
@@ -0,0 +1,147 @@
+define(function (require) {
+  'use strict';
+  var Layout = require('layoutmanager');
+  var $ = require('jquery');
+  var d3 = require('d3');
+
+  require('css!./chart');
+
+  var $window = $(window);
+
+  var ChartView = Layout.extend({
+    className: 'mc-chart',
+
+    manage: false,
+
+    initialize: function() {
+      this.listenTo(this.model, 'change', this.updateChart);
+      $window.on('resize', this.resize.bind(this));
+    },
+
+    serialize: function() {
+      return this.model.toJSON();
+    },
+
+    beforeRender: function () {
+      this.$container = $('.mc-chart-container');
+      this.drawChart();
+    },
+
+    resize: function() {
+      this.width = this.$container.width();
+      this.updateChart();
+    },
+
+    cleanup: function() {
+      this.svg.remove();
+      $window.off('resize');
+    },
+
+    drawChart: function() {
+      this.baseAmount = require('../../total');
+      this.data = [this.baseAmount];
+
+      this.width = this.$container.width();
+
+      this.svg = d3.select(this.el)
+        .append("svg")
+          .attr("width", this.width)
+          .attr("height", 150);
+
+      this.margin = 20;
+      this.startingMaximum = this.baseAmount * 5;
+
+      this.scale = d3.scale.linear()
+        .domain([0,this.startingMaximum])
+        .range([0, this.width]);
+
+      this.colors = d3.scale.category20c();
+
+      // Define the x-axis.
+      this.xAxis = d3.svg.axis()
+        .scale(this.scale)
+        .orient('bottom');
+
+      // Append the x-axis to the chart.
+      this.svg.append('g')
+        .attr('class', 'axis')
+        .attr('transform', 'translate(20,130)')
+        .call(this.xAxis);
+
+      this.updateChart()
+    },
+
+    updateScale: function() {
+      var sum = this.data.reduce(function (p, c) {
+        return p + c;
+      }, 0);
+      if (sum < 500000) sum = 500000;
+      this.scale = d3.scale.linear()
+        .domain([0,sum])
+        .range([0, this.width - this.margin * 2]);
+    },
+
+    updateChart: function() {
+      var _this = this;
+
+      this.data = [this.baseAmount]
+        .concat(this.model.toJSON().rounds.map(function (round) {
+          return round.excess;
+        }));
+
+      this.svg.attr('width', this.width);
+
+      this.updateScale();
+
+      this.currentPosition = 0;
+      var rects = this.svg.selectAll('rect');
+
+      // Enter new rectangles.
+      rects
+        .data(this.data)
+        .enter()
+        .append('rect')
+          .attr({
+            x: function (d, i) {
+              return  _this.scale(_this.data.slice(0, -1)
+                .reduce(function (a, b) {
+                  return a + b;
+                }, 0)) + _this.margin;
+            },
+            y: '10px',
+            width: function (d, i) {
+              return _this.scale(d);
+            },
+            height: '115px',
+            fill: function (d, i) {
+              return _this.colors(i);
+            }
+          });
+
+      rects.data(this.data)
+        .transition()
+          .duration(200)
+          .ease("linear")
+        .attr({
+          x: function (d, i) {
+            return  _this.scale(_this.data.slice(0, i)
+              .reduce(function (a, b) {
+                return a + b;
+              }, 0)) + _this.margin;
+          },
+          width: function (d, i) {
+            return _this.scale(d);
+          }
+        });
+
+      rects.data(this.data).exit().remove();
+
+      // Adjust the scale on the x-axis and update it.
+      this.xAxis.scale(this.scale);
+      this.svg.selectAll("g.axis")
+        .call(this.xAxis);
+    }
+  });
+
+  return ChartView;
+});
