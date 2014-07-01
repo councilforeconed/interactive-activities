301f959921fdf066d276893065a1fe654b0f8b47
Steve Kinney <hello@stevekinney.net>
2014-06-30 21:38:22 -0400

    Remove additional CSS dependencies

--- src/activities/pizza/client/components/workstation/cheese-workstation.js
+++ src/activities/pizza/client/components/workstation/cheese-workstation.js
@@ -4,8 +4,6 @@ define(function(require) {
   var ToppingsWorkstation = require('./toppings-workstation');
   var workstations = require('../../../shared/config').workstations;
 
-  require('css!./cheese-workstation');
-
   var CheeseWorkstation = ToppingsWorkstation.extend({
     toppingClass: 'pizza-topping-cheese',
     config: workstations.byId.cheese.config
